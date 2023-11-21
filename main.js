f7a12a7dd23fef8db182bda41781e98a20112fc7
enriquegaspar <105357875+enriquegaspar@users.noreply.github.com>
2023-11-21 07:45:06 -0300

    ok

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ function tocaSom(idElementoAudio){
 }
 //criando a referência constante listaDeTeclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
-//criando referência variável contador, atribuindo o valor inicial como "0"
-
 //Criando o código de repetição WHILE e mudando para FOR
-//enquanro o contador for menos que o comprimento da lista, tocará o som
+//enquanro o contador for menos que o comprimento da lista, tocará o som.
 for(let contador = 0;contador < listaDeTeclas.length;contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]; //chama a classe do indice 1 do som
